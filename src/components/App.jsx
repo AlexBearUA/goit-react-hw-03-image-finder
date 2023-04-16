58ccecbf91118f248ff9b296b8ba2bb7a0c7b929
Oleksandr Vedmedenko <stbearua@gmail.com>
2023-04-16 15:50:11 +0200

    +1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
+const axios = require('axios').default;
 // import ImagesApiService from '../services/images-api';
 class App extends Component {
   state = { searchQuery: '' };
 
-  componentDidMount() {
-    fetch(
-      `https://pixabay.com/api/?q=cat&page=1&key=34213016-753010ce7a0400954b4163a43&image_type=photo&orientation=horizontal&per_page=12`
-    )
-      .then(res => res.json)
-      .then(data => console.log(data));
+  async componentDidMount() {
+    const url = `https://pixabay.com/api/?q=cat&page=1&key=34213016-753010ce7a0400954b4163a43&image_type=photo&orientation=horizontal&per_page=12`;
+
+    const data = await axios.get(url);
+    console.log(data);
   }
 
   handleSearchQuery = searchQuery => {
